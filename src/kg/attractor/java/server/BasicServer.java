d5f7bc546553ff3a463711c87208e6c951c02206
Vladislav <cankerousrook7@gmail.com>
2026-06-30 18:55:47 +0600

    IDEA need to throw exception so added

--- src/kg/attractor/java/server/BasicServer.java
+++ src/kg/attractor/java/server/BasicServer.java
@@ -124,7 +124,7 @@ public abstract class BasicServer {
         }
     }
 
-    private void handleIncomingServerRequests(HttpExchange exchange) {
+    private void handleIncomingServerRequests(HttpExchange exchange) throws IOException {
         var route = getRoutes().getOrDefault(makeKey(exchange), this::respond404);
         route.handle(exchange);
     }
